45029f80d3588084638400f0be2dea6a8ee160bf
vnsemkin <vnsemkin@gmail.com>
2024-06-17 20:07:40 +0300

    Задание 7,8,9. Исправлены замечания.

--- src/main/java/org/vnsemkin/semkintelegrambot/application/dtos/CustomerRegistrationDto.java
+++ src/main/java/org/vnsemkin/semkintelegrambot/application/dtos/CustomerRegistrationDto.java
@@ -2,6 +2,6 @@ package org.vnsemkin.semkintelegrambot.application.dtos;
 
 import org.springframework.lang.NonNull;
 
-public record CustomerRegistrationDto(long tgId, @NonNull String firstName, @NonNull String username,
+public record CustomerRegistrationDto(long tgId, @NonNull String firstName, @NonNull String userName,
                                       @NonNull String email, @NonNull String password) {
 }
